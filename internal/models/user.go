f8f41acc3fa4d4121ac09af92b2bd29c155d16ea
g.petrakis <g.petrakis@natechbanking.com>
2025-03-14 14:05:45 +0200

    Feat: add User model with fields for ID, Username, Password, LastLogin, and CreatedAt

---
+++ internal/models/user.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"time"
+)
+
+// User represents the structure of a user
+type User struct {
+	ID        uint      `gorm:"primaryKey"`
+	Username  string    `gorm:"uniqueIndex;not null"`
+	Password  string    `gorm:"not null"`
+	LastLogin time.Time `gorm:"autoUpdateTime"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+}
